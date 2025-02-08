a1fad1de55c8391dafd3d9acdadcea82341f6cd6
pushfoo <36696816+pushfoo@users.noreply.github.com>
2025-02-08 06:16:48 -0500

    Add platform-native filepicker via plyer + fallback

--- pyMRI/loading/generic.py
+++ pyMRI/loading/generic.py
@@ -1,7 +1,31 @@
 from typing import Self, NamedTuple
 from pathlib import Path
 
-from tkinter import filedialog
+import logging
+log = logging.getLogger(Path(__file__).name)
+
+
+try: # Try to use native filepickers via plyer
+    import plyer
+    def askopenfilename(title, filetypes, initialdir) -> str:
+        """Plyer-based native file dialog binding.
+
+        On Linux, this has additional config options which are documented in the source,
+        but not on readthedocs at the moment. See the following for more:
+        https://github.com/kivy/plyer/blob/2.1.0/plyer/platforms/linux/filechooser.py
+        """
+        try:
+            data = plyer.filechooser.open_file(title=title, filetypes=filetypes, initialdir=initialdir)
+            log.debug(data)
+            return [data][0][0]
+        except Exception as e:
+            log.warning(f"Failed to pick file: {e}")
+            return None
+
+except ImportError as e:
+    log.warning("Failed to import file_chooser from plyer, falling back to tkinter")
+    from tkinter import filedialog
+    askopenfilename = filedialog.askopenfilename
 
 
 class FileLoader[T: NamedTuple]:
@@ -26,7 +50,7 @@ class FileLoader[T: NamedTuple]:
 
     @classmethod
     def fetch(cls, start_folder: Path = None) -> Self | None:
-        path_str = filedialog.askopenfilename(
+        path_str = askopenfilename(
             title=cls.dialog_title,
             filetypes=cls.accepted_file_types,
             initialdir=start_folder,
